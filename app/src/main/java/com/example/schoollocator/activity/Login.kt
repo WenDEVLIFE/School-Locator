a9cff074d48ff7866bb6e5a88abf13bc340ed36a
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-07-29 10:25:47 +0800

    Added icons on the username textfield 👾

--- app/src/main/java/com/example/schoollocator/activity/Login.kt
+++ app/src/main/java/com/example/schoollocator/activity/Login.kt
@@ -1,11 +1,13 @@
 package com.example.schoollocator.activity
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +17,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -27,12 +32,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.schoollocator.R
 import com.example.schoollocator.ui.theme.Green1
+import com.example.schoollocator.ui.theme.Purple80
 import com.example.schoollocator.ui.theme.SchoolLocatorTheme
 
 class Login : ComponentActivity() {
@@ -77,7 +84,6 @@ fun LoginForm1(modifier: Modifier = Modifier) {
             )
 
             Spacer(modifier = Modifier.height(26.dp))
-
             Text(
                 modifier = Modifier.align(Alignment.Start),
                 text = "Username",
@@ -87,13 +93,24 @@ fun LoginForm1(modifier: Modifier = Modifier) {
             )
 
             Spacer(modifier = Modifier.height(16.dp))
-
-            TextField(
+            OutlinedTextField(
                 value = username,
                 onValueChange = { username = it },
-                label = { Text("Enter your username") },
-                modifier = Modifier.fillMaxWidth().padding(14.dp)
+               placeholder = {
+                   Text(text = "Enter your username")
+               },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(14.dp)
                     .align(Alignment.Start)
+                    .background(Color.White),
+                leadingIcon = {
+                    Icon(modifier = Modifier.padding(end = 10.dp),
+                        painter = painterResource(id = R.drawable.baseline_person_24),
+                        contentDescription = "Icon",
+                        tint = Color.Black
+                        )
+                }
             )
         }
     }
