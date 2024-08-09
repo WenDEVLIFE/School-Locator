3cb84669dc6448e6d0f71c7d301bc965962019eb
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-08-09 20:54:32 +0800

    Change the background color and text color  whne user type 👾

--- app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/Map.kt
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/Map.kt
@@ -1,5 +1,6 @@
 package com.example.schoollocator.activity.maincomponent.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,9 +10,11 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,11 +22,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.schoollocator.BuildConfig
+import com.example.schoollocator.ui.theme.WhiteCus
+import com.example.schoollocator.windowEnum.ScreenSize
+import com.example.schoollocator.windowEnum.getScreenSize
 import com.mapbox.mapboxsdk.maps.MapView
 
 // This method is used to load the maps
@@ -54,6 +63,10 @@ fun Map(modifier: Modifier = Modifier,  OnClick: () -> Unit = {}) {
 
 @Composable
 fun MainMap(modifier: Modifier = Modifier, onClick: () -> Unit = {}) {
+
+    // Get the screen size
+    val screenSize = getScreenSize()
+
     // State to hold search query
     var query by remember { mutableStateOf("") }
 
@@ -76,12 +89,17 @@ fun MainMap(modifier: Modifier = Modifier, onClick: () -> Unit = {}) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar(
     query: String,
     onQueryChanged: (String) -> Unit,
     onSearch: () -> Unit
 ) {
+
+    // Get the screen size
+    val screenSize = getScreenSize()
+
     // Use TextField for the search bar
     TextField(
         value = query,
@@ -98,9 +116,21 @@ fun SearchBar(
         keyboardActions = KeyboardActions(
             onSearch = { onSearch() }
         ),
+
+        textStyle = TextStyle(
+            color = Color.Black,
+            fontSize = if (screenSize == ScreenSize.SMALL) 12.sp else 15.sp
+        ),
+
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = WhiteCus, // Set the background color here
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
+
     )
 }
 
