2f9d78c5898a40db7e1552e5a0801f61aad6b265
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-08-18 16:03:09 +0800

    Added search bar on the UserScreen.kt 👾

--- app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/UserScreen.kt
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/UserScreen.kt
@@ -1,11 +1,32 @@
 package com.example.schoollocator.activity.maincomponent.ui
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 
 // TODO: Implement the UserScreen composable function
 fun UserScreen() {
 
+    // This is the state of the search query
+    val query = remember { mutableStateOf("") }
 
+    SearchBar(
+        query = query.value,
+        onQueryChanged = { newQuery ->query.value = newQuery },
+        onSearch = { /* Handle search action here */ }
+    )
+    LazyColumn {
+
+    }
+
+}
+
+@Preview
+@Composable
+fun PreviewUserScreen() {
+    UserScreen()
 }
