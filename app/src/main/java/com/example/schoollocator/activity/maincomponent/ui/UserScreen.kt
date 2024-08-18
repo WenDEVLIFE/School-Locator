80160105b4883cd3e39951cba22760e2be880ed0
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-08-19 07:58:51 +0800

    Added Topbar on the HomeScreen 👾

--- app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/UserScreen.kt
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/UserScreen.kt
@@ -2,18 +2,32 @@ package com.example.schoollocator.activity.maincomponent.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.schoollocator.ui.theme.Typography
 import com.example.schoollocator.ui.theme.lightgreen
+import com.example.schoollocator.ui.theme.materialGreen
+import com.example.schoollocator.ui.theme.materialLightGreen
+import com.example.schoollocator.windowEnum.ScreenSize
+import com.example.schoollocator.windowEnum.getScreenSize
 
 @Composable
 
@@ -27,11 +41,14 @@ fun UserScreen() {
         modifier = Modifier
         .background(lightgreen)
     ) {
+        TopAppBarState(modifier = Modifier)
+        Spacer(modifier = Modifier.size(10.dp))
         SearchBar(
             query = query.value,
             onQueryChanged = { newQuery ->query.value = newQuery },
             onSearch = { /* Handle search action here */ }
         )
+        Spacer(modifier = Modifier.size(10.dp))
         UserList(modifier = Modifier.weight(1f))
     }
 
@@ -52,6 +69,20 @@ fun UserList(modifier: Modifier) {
         }
 
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarState(modifier: Modifier) {
+    val screenSize = getScreenSize()
+
+    TopAppBar(
+        title = { Text(text = "User", style = Typography.bodySmall, fontSize = if (screenSize == ScreenSize.SMALL) 25.sp else 35.sp, color = materialGreen) },
+        modifier = modifier,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = materialLightGreen
+        )
+    )
 }
 
 @Preview
