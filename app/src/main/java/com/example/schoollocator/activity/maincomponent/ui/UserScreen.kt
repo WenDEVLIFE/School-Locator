6d898592859a2361b8a48da9bbe48eba9096c62a
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-08-18 15:41:02 +0800

    Added Person Add 👾

--- app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/UserScreen.kt
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/UserScreen.kt
@@ -3,6 +3,9 @@ package com.example.schoollocator.activity.maincomponent.ui
 import androidx.compose.runtime.Composable
 
 @Composable
+
+// TODO: Implement the UserScreen composable function
 fun UserScreen() {
+
 
 }
