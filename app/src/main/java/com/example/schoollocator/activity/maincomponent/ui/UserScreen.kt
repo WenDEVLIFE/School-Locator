e2bdb8fe3f67de51d9565f1b4fb22e0a61830a2b
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-08-18 16:10:07 +0800

    Connect the column and the SearchBar 👾

--- app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/UserScreen.kt
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/UserScreen.kt
@@ -1,10 +1,19 @@
 package com.example.schoollocator.activity.maincomponent.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.schoollocator.ui.theme.lightgreen
 
 @Composable
 
@@ -14,15 +23,35 @@ fun UserScreen() {
     // This is the state of the search query
     val query = remember { mutableStateOf("") }
 
-    SearchBar(
-        query = query.value,
-        onQueryChanged = { newQuery ->query.value = newQuery },
-        onSearch = { /* Handle search action here */ }
-    )
-    LazyColumn {
-
+    Column(
+        modifier = Modifier
+        .background(lightgreen)
+    ) {
+        SearchBar(
+            query = query.value,
+            onQueryChanged = { newQuery ->query.value = newQuery },
+            onSearch = { /* Handle search action here */ }
+        )
+        UserList(modifier = Modifier.weight(1f))
     }
 
+
+}
+
+@Composable
+fun UserList(modifier: Modifier) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize() // Ensure LazyColumn fills the remaining space
+            .padding(11.dp)
+            .background(lightgreen),
+        verticalArrangement = Arrangement.spacedBy(8.dp) // Add spacing between items
+    ) {
+        items(100) {
+            Text(text = "Item $it")
+        }
+
+    }
 }
 
 @Preview
