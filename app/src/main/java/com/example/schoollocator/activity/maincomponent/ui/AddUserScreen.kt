eff6ae44c2f0b726e4e5f5e20be60036c9d6ae7e
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-08-20 11:05:20 +0800

    Added AddScreen 👾

---
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/AddUserScreen.kt
@@ -0,0 +1,20 @@
+package com.example.schoollocator.activity.maincomponent.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+// This is the AddUserScreen
+@Composable
+fun AddUser(modifier: Modifier=Modifier){
+
+}
+
+
+// This is the preview of the AddUserScreen
+@Preview
+@Composable
+fun AddUserPreview(){
+    AddUser()
+}
