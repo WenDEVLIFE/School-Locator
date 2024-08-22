a5a049c5bdec497b34901856c0b5069b4586b9c2
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-08-22 09:09:12 +0800

    Added add user form and connect on the main compose of AddUserScreen.kt 👾

--- app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/AddUserScreen.kt
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/AddUserScreen.kt
@@ -1,11 +1,19 @@
 package com.example.schoollocator.activity.maincomponent.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.schoollocator.ui.theme.Green1
 import com.example.schoollocator.ui.theme.lightgreen
 
 
@@ -20,7 +28,30 @@ fun AddUser(modifier: Modifier=Modifier){
 
         // Top bar state
         TopAppBarState(modifier =Modifier, tittle ="Add User")
+        AddUserForm()
 
+
+    }
+
+}
+
+@Composable
+fun AddUserForm(modifier: Modifier=Modifier){
+    Box(   modifier = modifier
+        .fillMaxSize()
+        .background(lightgreen)
+        .padding(16.dp)){
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+
+        ) {
+            items(10){
+                Text(text = "Hello")
+            }
+
+        }
     }
 
 }
