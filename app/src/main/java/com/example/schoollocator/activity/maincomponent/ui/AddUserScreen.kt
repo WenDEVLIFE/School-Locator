2c6e23aa66bc5794078c71060b5280d62ecba844
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-08-22 09:26:11 +0800

    Added minor ui to the AddUserScreen.kt 👾

--- app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/AddUserScreen.kt
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/AddUserScreen.kt
@@ -4,17 +4,38 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.schoollocator.R
 import com.example.schoollocator.ui.theme.Green1
 import com.example.schoollocator.ui.theme.lightgreen
+import com.example.schoollocator.windowEnum.ScreenSize
+import com.example.schoollocator.windowEnum.getScreenSize
 
 
 // This is the AddUserScreen
@@ -37,20 +58,132 @@ fun AddUser(modifier: Modifier=Modifier){
 
 @Composable
 fun AddUserForm(modifier: Modifier=Modifier){
-    Box(   modifier = modifier
-        .fillMaxSize()
-        .background(lightgreen)
-        .padding(16.dp)){
-        LazyColumn(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.padding(16.dp)
 
-        ) {
-            items(10){
-                Text(text = "Hello")
-            }
+    val screenSize = getScreenSize()
+    val username  = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
 
+
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize() // Ensure LazyColumn fills the remaining space
+            .padding(11.dp)
+            .background(lightgreen),
+        verticalArrangement = Arrangement.spacedBy(8.dp) // Add spacing between items
+    ) {
+        item {
+            Spacer(modifier = Modifier.height(26.dp))
+        }
+
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp),
+                text = "Username",
+                fontFamily = MaterialTheme.typography.bodyLarge.fontFamily,
+                color = Color.White,
+                fontSize = if (screenSize == ScreenSize.SMALL) 22.sp else 25.sp
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+
+        item {
+            TextField(
+                value = username.value,
+                onValueChange = {  username.value = it },
+                placeholder = { Text(text = "Enter your username") },
+                shape = RoundedCornerShape(20.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(14.dp)
+                    .background(Color.Transparent),
+                textStyle = TextStyle(
+                    color = Color.Black,
+                    fontSize = if (screenSize == ScreenSize.SMALL) 12.sp else 15.sp
+                ),
+                leadingIcon = {
+                    Icon(
+                        modifier = Modifier.padding(end = 10.dp),
+                        painter = painterResource(id = R.drawable.baseline_person_24),
+                        contentDescription = "Icon",
+                        tint = Color.Black
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp),
+                text = "Password",
+                fontFamily = MaterialTheme.typography.bodyLarge.fontFamily,
+                color = Color.White,
+                fontSize = if (screenSize == ScreenSize.SMALL) 22.sp else 25.sp
+            )
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(5.dp))
+        }
+
+        item {
+            TextField(
+                value = password.value,
+                onValueChange = { password.value = it) },
+                placeholder = { Text(text = "Enter your password") },
+                shape = RoundedCornerShape(20.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(14.dp)
+                    .background(Color.Transparent),
+                textStyle = TextStyle(color = Color.Black, fontSize = 20.sp),
+                leadingIcon = {
+                    Icon(
+                        modifier = Modifier.padding(end = 10.dp),
+                        painter = painterResource(id = R.drawable.baseline_key_24),
+                        contentDescription = "Icon",
+                        tint = Color.Black
+                    )
+                },
+                trailingIcon = {
+                    val image = if (viewModel.isPasswordVisible.value)
+                        painterResource(id = R.drawable.see)
+                    else
+                        painterResource(id = R.drawable.eye)
+
+                    IconButton(onClick = { viewModel.passwordToogle() }) {
+                        Icon(
+                            painter = image,
+                            contentDescription = "Toggle password visibility",
+                            modifier = Modifier.size(
+                                width = if (screenSize == ScreenSize.SMALL) 20.dp else 24.dp,
+                                height = if (screenSize == ScreenSize.SMALL) 20.dp else 24.dp
+                            )
+                        )
+                    }
+                },
+                visualTransformation = if (viewModel.isPasswordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
         }
     }
 
