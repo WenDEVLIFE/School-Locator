56f3e94734ed80ec007dae4eb9065d4ba32adca0
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-09-04 09:18:56 +0800

    Added CreatedSchoolScreen

---
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/CreatedSchoolScreen.kt
@@ -0,0 +1,11 @@
+package com.example.schoollocator.activity.maincomponent.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun CreatedSchoolScreen(modifier: Modifier = Modifier, navController:NavHostController) {
+
+}
