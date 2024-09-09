8496fd546df5b122216eb84a41dfdf0f15c6efae
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-09-09 15:13:29 +0800

    Added MessageScreen 👾

---
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/MessageScreen.kt
@@ -0,0 +1,8 @@
+package com.example.schoollocator.activity.maincomponent.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MessageScreen(){
+    //TODO : Add the message screen here
+}
