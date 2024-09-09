21b2ca942ae94df5c7a99cd74449edf5c2c7e62a
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-09-10 07:57:48 +0800

    Added ChangeProfileScreen.kt .👾

---
+++ app/src/main/java/com/example/schoollocator/activity/maincomponent/ui/ChangeProfileScreen.kt
@@ -0,0 +1,8 @@
+package com.example.schoollocator.activity.maincomponent.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ChangeProfileScreen(){
+    //TODO : Add the change profile screen here
+}
